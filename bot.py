a322001454c0143ec4bc077c5715187ed558b374
quickB <oshustro@gmail.com>
2022-05-05 22:50:31 +0300

    Update bot.py
    some comments in code
    now /Random send gifs and large files (as document)
    some fix

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ from subprocess import call
 import os
 import glob
 import shutil
-import natsort
 
 from config import TOKEN
 
@@ -36,6 +35,7 @@ async def process_help_command(message: types.Message):
 @dp.message_handler(commands=['Random'])
 async def process_start_command(message: types.Message):
     await message.answer("загружаю картинки")
+    # задаются типы альбомов для отправки
     mediaA = types.MediaGroup()
     mediaB = types.MediaGroup()
     mediaC = types.MediaGroup()
@@ -43,107 +43,129 @@ async def process_start_command(message: types.Message):
     mediaE = types.MediaGroup()
     mediaF = types.MediaGroup()
     mediaG = types.MediaGroup()
+
+    # peer это префикс в имени файла,означающий принадлежность к альбому
+    # потом я всё переделаю без буквенных префиксов, а пока так
+    # сокральное значение "Т" состоит в том, что оно не из диапазона A-G
     peer = 'T'
     file_source = './image/'
+
+    # запускаем скрипт скачивания картинок с рандомной страницы и забираем счетчик картинок
     p = subprocess.Popen(['./jreactor_random_page.sh'], stdout=subprocess.PIPE)
     count_files = int(p.stdout.readline())
+
+    # если картинок больше десятков на 1 (например, 11,21,31 и т.д.) то последняя картинка
+    # сохраняется в отдельную папку и докидывается потом отдельным сообщением, т.к. альбомы работают с 2-10
     ost_cf = count_files % 10
     await message.answer("картинки загружены, распихиваю по альбомам")
     if ost_cf == 1:
+        # для перемещения одного файла написал скрипт на баше)
+        # потом на питоне напишу строку
         scr = subprocess.Popen(['./move_last_file.sh'], stdout=subprocess.PIPE)
         wait_sh = scr.stdout.readline()
+
+    # тутнадо переделать. получаем список файлов
     name_of_files = [os.path.basename(x) for x in glob.glob('./image/*')]
-    name_of_files.pop()
+    # удаляем имена папок (надо в одну строку написать, чтоли)
+    name_of_files.remove('last')
+    name_of_files.remove('gif')
+    name_of_files.remove('doc')
+    # сортируем по счетчику (чтоб вместо 1,10,2,3...9 было 1,2,3,4...10)
     name_of_files=natsorted(name_of_files)
-    print(name_of_files)
-    for file in name_of_files:
-        if file[0] == 'A':
-            mediaA.attach_photo(types.InputFile(file_source + file))
-            peer = 'A'
-            print(file)
-        elif file[0] == 'B':
-            mediaB.attach_photo(types.InputFile(file_source + file))
-            peer = 'B'
-            print(file)
-        elif file[0] == 'C':
-            mediaC.attach_photo(types.InputFile(file_source + file))
-            peer = 'C'
-            print(file)
-        elif file[0] == 'D':
-            mediaD.attach_photo(types.InputFile(file_source + file))
-            peer = 'D'
-            print(file)
-        elif file[0] == 'E':
-            mediaE.attach_photo(types.InputFile(file_source + file))
-            peer = 'E'
-            print(file)
-        elif file[0] == 'F':
-            mediaF.attach_photo(types.InputFile(file_source + file))
-            peer = 'F'
-            print(file)
-        elif file[0] == 'G':
-            mediaG.attach_photo(types.InputFile(file_source + file))
-            peer = 'G'
-            print(file)
-    if peer == 'A':
-        #print(mediaA)
-        await message.answer_media_group(media=mediaA)
-    elif peer == 'B':
-        await message.answer_media_group(media=mediaA)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaB)
-    elif peer == 'C':
-        await message.answer_media_group(media=mediaA)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaB)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaC)
-    elif peer == 'D':
-        await message.answer_media_group(media=mediaA)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaB)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaC)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaD)
-    elif peer == 'E':
-        await message.answer_media_group(media=mediaA)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaB)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaC)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaD)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaE)
-    elif peer == 'F':
-        await message.answer_media_group(media=mediaA)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaB)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaC)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaD)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaE)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaF)
-    elif peer == 'G':
-        await message.answer_media_group(media=mediaA)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaB)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaC)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaD)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaE)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaF)
-        time.sleep(1)
-        await message.answer_media_group(media=mediaG)
+
+    # это строка для дебага, можно удалить
+    # print(name_of_files)
+
+    if name_of_files:
+        # тут файлы распихиваются по альбомам в зависимости от префикса
+        for file in name_of_files:
+            if file[0] == 'A':
+                mediaA.attach_photo(types.InputFile(file_source + file))
+                peer = 'A'
+            elif file[0] == 'B':
+                mediaB.attach_photo(types.InputFile(file_source + file))
+                peer = 'B'
+            elif file[0] == 'C':
+                mediaC.attach_photo(types.InputFile(file_source + file))
+                peer = 'C'
+            elif file[0] == 'D':
+                mediaD.attach_photo(types.InputFile(file_source + file))
+                peer = 'D'
+            elif file[0] == 'E':
+                mediaE.attach_photo(types.InputFile(file_source + file))
+                peer = 'E'
+            elif file[0] == 'F':
+                mediaF.attach_photo(types.InputFile(file_source + file))
+                peer = 'F'
+            elif file[0] == 'G':
+                mediaG.attach_photo(types.InputFile(file_source + file))
+                peer = 'G'
+
+        # а тут пошла жара))) да, это надо переделать когда избавлюсь от префиксов
+        # но работает. тайм слип посавил, с ним телеграм бота реже банит за спам в лс пользователю
+        # у меня прилетал до 4х раз в день на 9 секунд каждый. с задержкой пока только раз
+        if peer == 'A':
+            await message.answer_media_group(media=mediaA)
+        elif peer == 'B':
+            await message.answer_media_group(media=mediaA)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaB)
+        elif peer == 'C':
+            await message.answer_media_group(media=mediaA)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaB)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaC)
+        elif peer == 'D':
+            await message.answer_media_group(media=mediaA)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaB)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaC)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaD)
+        elif peer == 'E':
+            await message.answer_media_group(media=mediaA)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaB)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaC)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaD)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaE)
+        elif peer == 'F':
+            await message.answer_media_group(media=mediaA)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaB)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaC)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaD)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaE)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaF)
+        elif peer == 'G':
+            await message.answer_media_group(media=mediaA)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaB)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaC)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaD)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaE)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaF)
+            time.sleep(1)
+            await message.answer_media_group(media=mediaG)
+        else:
+            await message.answer("что-то пошло не так")
     else:
-        await message.answer("что-то пошло не так")
+        await message.answer("картинок нет")
+
+
     #докидываем анимации, если они были
     dir_gif = './image/gif'
     files = os.listdir(dir_gif)
@@ -158,12 +180,44 @@ async def process_start_command(message: types.Message):
             await message.answer_animation(video)
             video.close()
 
+    # докидываем последнюю картинку, если есть
+    # надо переделать. цикл не нужен. она всегда одна
+    last_dir = './image/last'
+    last_files = os.listdir(last_dir)
+    time.sleep(1)
+    if last_files != '':
+        dir_for_add = './image/last/'
+        files_for_add = []
+        for file in last_files:
+            files_for_add.append(dir_for_add + file)
+        for var in files_for_add:
+            photo = open(var, 'rb')
+            await message.answer_photo(photo)
+            photo.close()
+
+    # докидываем картинки больше 5мб (если есть) как документы
+    as_doc = './image/doc'
+    doc_files = os.listdir(as_doc)
+    time.sleep(1)
+    if doc_files != '':
+        dir_for_add = './image/doc/'
+        files_for_add = []
+        for file in doc_files:
+            files_for_add.append(dir_for_add + file)
+        for var in files_for_add:
+            big_file = open(var, 'rb')
+            await message.answer_document(big_file)
+            big_file.close()
 
 @dp.message_handler()
 async def echo_message(message: types.Message):
+    # забираем текст сообщения пользователя как тег
     tag_react = message.text
+    # перекодируем для URL если текст на русском
     tag_for_curl = urllib.parse.quote(tag_react)
     await message.answer("загружаю картинки")
+
+    # задаются типы альбомов для отправки
     mediaA = types.MediaGroup()
     mediaB = types.MediaGroup()
     mediaC = types.MediaGroup()
@@ -171,51 +225,66 @@ async def echo_message(message: types.Message):
     mediaE = types.MediaGroup()
     mediaF = types.MediaGroup()
     mediaG = types.MediaGroup()
+
+    # peer это префикс в имени файла,означающий принадлежность к альбому
+    # потом я всё переделаю без буквенных префиксов, а пока так
+    # сокральное значение "Т" состоит в том, что оно не из диапазона A-G
     peer = 'T'
     file_source = './image/'
+
+    # запускаем скрипт скачивания картинок и передаём в аргумент тег из сообщения пользователя
+    # забираем счетчик картинок
     p = subprocess.Popen(['./jreactor_tag_page.sh {0}'.format(tag_for_curl)], stdout=subprocess.PIPE, shell=True)
     count_files = int(p.stdout.readline())
+
+    # если картинок больше десятков на 1 (например, 11,21,31 и т.д.) то последняя картинка
+    # сохраняется в отдельную папку и докидывается потом отдельным сообщением, т.к. альбомы работают с 2-10
     ost_cf = count_files % 10
     await message.answer("картинки загружены, распихиваю по альбомам")
     if ost_cf == 1:
+        # для перемещения одного файла написал скрипт на баше)
+        # потом на питоне напишу строку
         scr = subprocess.Popen(['./move_last_file.sh'], stdout=subprocess.PIPE)
         wait_sh = scr.stdout.readline()
+
+    # тутнадо переделать. получаем список файлов
     name_of_files = [os.path.basename(x) for x in glob.glob('./image/*')]
+    # удаляем имена папок (надо в одну строку написать, чтоли)
     name_of_files.remove('last')
     name_of_files.remove('gif')
     name_of_files.remove('doc')
+    # сортируем по счетчику (чтоб вместо 1,10,2,3...9 было 1,2,3,4...10)
     name_of_files = natsorted(name_of_files)
-    print(name_of_files)
+    
+
     if name_of_files:
+        # тут файлы распихиваются по альбомам в зависимости от префикса
         for file in name_of_files:
             if file[0] == 'A':
                 mediaA.attach_photo(types.InputFile(file_source + file))
                 peer = 'A'
-                print(file)
             elif file[0] == 'B':
                 mediaB.attach_photo(types.InputFile(file_source + file))
                 peer = 'B'
-                print(file)
             elif file[0] == 'C':
                 mediaC.attach_photo(types.InputFile(file_source + file))
                 peer = 'C'
-                print(file)
             elif file[0] == 'D':
                 mediaD.attach_photo(types.InputFile(file_source + file))
                 peer = 'D'
-                print(file)
             elif file[0] == 'E':
                 mediaE.attach_photo(types.InputFile(file_source + file))
                 peer = 'E'
-                print(file)
             elif file[0] == 'F':
                 mediaF.attach_photo(types.InputFile(file_source + file))
                 peer = 'F'
-                print(file)
             elif file[0] == 'G':
                 mediaG.attach_photo(types.InputFile(file_source + file))
                 peer = 'G'
-                print(file)
+
+        # а тут пошла жара))) да, это надо переделать когда избавлюсь от префиксов
+        # но работает. тайм слип посавил, с ним телеграм бота реже банит за спам в лс пользователю
+        # у меня прилетал до 4х раз в день на 9 секунд каждый. с задержкой пока только раз
         if peer == 'A':
             # print(mediaA)
             await message.answer_media_group(media=mediaA)
